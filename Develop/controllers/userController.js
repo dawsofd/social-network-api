d481cb362327757d7040e4b821127bee3c6fce1e
Dawson Dohlen <d.dohlen@pm.me>
2024-12-16 16:04:24 -0800

    add user routes to userController

--- Develop/controllers/userController.js
+++ Develop/controllers/userController.js
@@ -1,31 +1,117 @@
-const { Schema, Types } = require('mongoose');
+const { User, Thought } = require("../models");
 
-const userSchema = new Schema(
-    {
-        username: {
-          type: String,
-          required: true,
-          unique: true,
-          trim: true
-        },
-        email: {
-          type: String,
-          required: true,
-          unique: true,
-          validate: [validateEmail, 'Please provide a valid email address'],
-          match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
-        },
-        thoughts: {
-        },
-        friends: {
-        },
-      },
-      {
-        toJSON: {
-          getters: true,
-        },
-        id: false,
-      }
-    );
-    
-    module.exports = userSchema;
+const userController = {
+    async getUsers(req, res) {
+        try {
+            const users = await User.find()
+                .populate({ path: "thoughts", select: "-__v" })
+                .populate({ path: "friends", select: "-__v" });
+            return res.status(200).json(users);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async getUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .populate({ path: "thoughts", select: "-__v" })
+                .populate({ path: "friends", select: "-__v" });
+            
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID found "});
+            }
+
+            return res.status(200).json(user)
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            return res.status(200).json(user);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID found" });
+            }
+
+            return res.status(200).json(user);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID found" });
+            }
+
+            await Thought.deleteMany({ _id: { $in: user.thoughts } });
+            return res.status(200).json({ message: "User and all thoughts deleted" });
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async addFriend(req, res) {
+        try {
+            const friend = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!friend) {
+                return res.status(404).json({ message: "No user with that ID found" });
+            }
+
+            return res.status(200).json(friend);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    },
+
+    async deleteFriend(req, res) {
+        try {
+            const friend = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!friend) {
+                return res.status(404).json({ message: "No user with that ID found" });
+            }
+
+            return res.status(200).json(friend);
+        } catch (err) {
+          console.log(err);
+          return res.status(500).json(err);
+        }
+    }
+};
+
+module.exports = userController;
